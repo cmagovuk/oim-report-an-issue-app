eedb19cd48e44257a218e158f412416e80959fa7
Chris Harvey <chris.harvey@deltascheme.com>
2021-12-15 10:55:00 +0000

    Removed turbolinks

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -2,10 +2,8 @@ require.context('govuk-frontend/govuk/assets');
 
 import '../styles/application.scss';
 import Rails from 'rails-ujs';
-//import Turbolinks from 'turbolinks';
 import { initAll } from 'govuk-frontend';
 import '../src/uploadDocs';
 
 Rails.start();
-//Turbolinks.start();
 initAll();
